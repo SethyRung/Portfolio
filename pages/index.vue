da074d13f6cf1136d0f81314e4f4e0fd2b32a5a0
SethyRung <rungsethyhk@gmail.com>
2025-03-02 18:45:21 +0700

    feat: add About-me section

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <HeroSection />
+    <AboutMeSection />
   </div>
 </template>
 <script setup lang="ts">
 import HeroSection from "~/components/HeroSection.vue";
+import AboutMeSection from "~/components/AboutMeSection.vue";
 </script>
