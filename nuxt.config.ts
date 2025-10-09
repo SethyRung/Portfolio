24a13c5d8b488808f8d12799947cd351c0b57858
SethyRung <rungsethyhk@gmail.com>
2025-10-09 22:21:37 +0700

    chore: config @nuxt/test for project testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-27",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts", "@vueuse/nuxt"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/fonts", "@vueuse/nuxt", '@nuxt/test-utils/module',],
   vite: {
     plugins: [tailwindcss()],
   },
